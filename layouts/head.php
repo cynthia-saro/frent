4b15aad25cc3d011f6c005436d411a589af38e1f
saro <cyn.saro@gmail.com>
2019-11-28 22:48:45 +0100

    Header

--- layouts/head.php
+++ layouts/head.php
@@ -7,9 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Frent - la location entre collègues</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
     <script src="https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js"></script>
     <!--Bootstrap-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
   <!----fermeture HEAD---->
--- layouts/header.php
+++ layouts/header.php
@@ -1,31 +1,36 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+  <button class="navbar-toggler headerIcons" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <img src="img/menu.png">
   </button>
 
   <a class="navbar-brand logo" href="#"><img src="img/frent.png"></a>
 
+  <a class="headerIcons" href="#">
+    <img src="img/user-symbol.png">
+  </a>
+
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="http://localhost/mspr1-nation-sounds/programme.html">Programme</a>
+        <a class="nav-link" href="#">Mes objets</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="mspr1-nation-sounds/geoloc.html">Carte</a>
+        <a class="nav-link" href="#">Mes réservations</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="https://www.digitick.com/festival-tickets">Billeterie</a>
+        <a class="nav-link" href="#">Mes favoris</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="http://nation-sounds.planethoster.world/faq">FAQ</a>
+        <a class="nav-link" href="#">Mes demandes ?</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="./partenaire.html">Partenaires</a>
+        <a class="nav-link" href="#">Mon groupe</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">FAQ</a>
       </li>
     </ul>
   </div>
-  
-  <a class="navbar-brand logo" href="#"><img src="img/frent.png"></a>
-  
+
 </nav>
